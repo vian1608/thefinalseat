d85f82490b5089450f6d8ce264ce8246f1263c50
vian1608 <70315309+vian1608@users.noreply.github.com>
2026-08-14 08:49:22 +0530

    Strengthen crawlable internal links

--- frontend/src/shared/components/Footer.js
+++ frontend/src/shared/components/Footer.js
@@ -36,10 +36,30 @@ function Footer() {
 
           <FooterSection title="Company">
             <ul>
-              <li><Link to="/">Flights</Link></li>
+              <li><Link to="/">Flight Search</Link></li>
               <li><Link to="/car-rentals">Car Rentals</Link></li>
               <li><Link to="/my-bookings">My Bookings</Link></li>
               <li><Link to="/contact">Contact Information</Link></li>
+            </ul>
+          </FooterSection>
+
+          <FooterSection title="Travel Help">
+            <ul>
+              <li><Link to="/travel-assistance">Flight Booking Assistance</Link></li>
+              <li><Link to="/booking-for-parents">Booking Flights for Parents</Link></li>
+              <li><Link to="/urgent-travel">Urgent Travel Assistance</Link></li>
+              <li><Link to="/senior-travel/flight-deals">Senior Flight Assistance</Link></li>
+            </ul>
+          </FooterSection>
+
+          <FooterSection title="Popular Routes">
+            <ul>
+              <li><Link to="/flight-nyc-to-mia">New York to Miami Flights</Link></li>
+              <li><Link to="/flight-lax-to-jfk">Los Angeles to New York Flights</Link></li>
+              <li><Link to="/routes/flight-nyc-to-lon">New York to London Flights</Link></li>
+              <li><Link to="/routes/flight-lax-to-tokyo">Los Angeles to Tokyo Flights</Link></li>
+              <li><Link to="/train-nyc-to-dc">New York to Washington Train</Link></li>
+              <li><Link to="/train-boston-to-nyc">Boston to New York Train</Link></li>
             </ul>
           </FooterSection>
 
