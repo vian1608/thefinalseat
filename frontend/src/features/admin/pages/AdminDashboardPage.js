5cda56b1e6131c5a3982855b76edbadc25910c7e
vian1608 <70315309+vian1608@users.noreply.github.com>
2026-08-21 00:54:13 +0530

    Remove VGS secure card panel from admin booking detail

--- frontend/src/features/admin/pages/AdminDashboardPage.js
+++ frontend/src/features/admin/pages/AdminDashboardPage.js
@@ -5,7 +5,6 @@ import AdminDashboardPageV2 from './AdminDashboardPageV2';
 import AdminBookingWorkspace from '../components/AdminBookingWorkspace';
 import AdminBookingAddressPanel from '../components/AdminBookingAddressPanel';
 import AdminBookingManagementPanel from '../components/AdminBookingManagementPanel';
-import AdminSecurePaymentPanel from '../components/AdminSecurePaymentPanel';
 import './AdminDashboardEnhancements.css';
 
 const ADMIN_BOOKINGS_PAGE_SIZE = 20;
@@ -183,7 +182,6 @@ export default function AdminDashboardPage() {
           <AdminBookingAddressPanel />
           <AdminBookingWorkspace />
           <AdminBookingManagementPanel />
-          <AdminSecurePaymentPanel />
         </>
       ) : (
         <AdminDashboardPageV2 />
